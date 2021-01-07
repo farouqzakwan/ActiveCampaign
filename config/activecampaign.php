24f5ab97e2ba89d46744edfbd55868c22d36603c
farouqzakwan <farouqzakwan@fatberry.com>
2021-01-08 00:43:45 +0800

    change constants to config array

--- config/activecampaign.php
+++ config/activecampaign.php
@@ -1,9 +1,12 @@
 <?php
-define('ACTIVECAMPAIGN_CONTACT_SUBSCRIBE_LIST',1);
-define('ACTIVECAMPAIGN_CONTACT_UNSUBSCRIBE_LIST',2);
-define('ACTIVECAMPAIGN_CONTACT_RESUBSCRIBE_LIST',4);
-
 return [
     'activecampaign_url'    => env('ACTIVECAMPAIGN_URL') ?? false,
     'activecampaign_key'    => env('ACTIVECAMPAIGN_KEY') ?? false,
+    'ids' => [
+        'list'  => [
+            'contact_subscribe' => 1,
+            'contact_unsubscribe' => 2,
+            'contact_resubscribe' => 4
+        ]
+    ]
 ];
